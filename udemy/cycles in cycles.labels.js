2801dc63262ec8b8106993f108fd3ace1c55792c
Victoria Tsykunova <vika.tsykunova02@gmail.com>
2022-08-04 17:36:48 +0300

    cycles

--- udemy/cycles in cycles.labels.js
+++ udemy/cycles in cycles.labels.js
@@ -9,14 +9,27 @@
 // }
 
 
-let result = '';
-const length = 7;
+// let result = '';
+// const length = 7;
 
-for ( let i = 1; i < length; i++) {
-for (let j = 0; j < i; i++){
-    result += "*";
-}
-    result += 'n'
-}
+// for ( let i = 1; i < length; i++) {
+// for (let j = 0; j < i; j++){
+//     result += "*";
+// }
+//     result += '\n';
+// }
 
-console.log(result);
+// console.log(result);
+
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) break first;
+            console.log(`Third level: ${k}`);
+            
+        }
+    }
+}
